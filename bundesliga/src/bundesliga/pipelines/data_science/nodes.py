63dd774683d5d6be5ef80968ffb163edbcfa41f8
PhilippDahlke <Philipp_dahlke@gmx.de>
2024-12-20 20:42:46 +0100

    parameters added to configure different models

--- bundesliga/src/bundesliga/pipelines/data_science/nodes.py
+++ bundesliga/src/bundesliga/pipelines/data_science/nodes.py
@@ -2,10 +2,11 @@
 This is a boilerplate pipeline 'data_science'
 generated using Kedro 0.19.10
 """
+import typing as t
+
 import arviz as az
 import netCDF4
 import pandas as pd
-import xarray as xr
 
 # from kedro_datasets_experimental.netcdf import NetCDFDataset
 from modelbuilder import FootballModel
@@ -20,13 +21,16 @@ from modelbuilder import FootballModel
 # average_goals = 3.0
 
 
-def model_1(model_input_table: pd.DataFrame) -> netCDF4.Dataset:
-    x_input = model_input_table[["home_id", "away_id"]]
-    goals = model_input_table[["home_goals", "away_goals"]].apply(
+def model_1(model_input_table: pd.DataFrame, parameters: t.Dict) -> netCDF4.Dataset:
+    x_input = model_input_table[parameters["features"]]
+    goals = model_input_table[parameters["targets"]].apply(
         lambda row: (row["home_goals"], row["away_goals"]), axis=1
     )
 
-    model = FootballModel()
+    model = FootballModel(
+        model_config=parameters["model_config"],
+        sampler_config=parameters["sampler_config"],
+    )
     model_1_idata = model.fit(
         X=x_input,
         y=goals,
--- bundesliga/src/bundesliga/pipelines/data_science/pipeline.py
+++ bundesliga/src/bundesliga/pipelines/data_science/pipeline.py
@@ -9,13 +9,26 @@ from .nodes import model_1
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline(
+    pipeline_instance = pipeline(
         [
             node(
                 func=model_1,
-                inputs="model_input_table",
+                inputs=["model_input_table", "params:model_parameters"],
                 outputs="model_1_idata",
                 name="model_1_node",
             ),
         ]
     )
+    ds_pipeline_1 = pipeline(
+        pipe=pipeline_instance,
+        inputs="model_input_table",
+        namespace="active_modelling_pipeline",
+    )
+
+    ds_pipeline_2 = pipeline(
+        pipe=pipeline_instance,
+        inputs="model_input_table",
+        namespace="candidate_modelling_pipeline",
+    )
+
+    return ds_pipeline_1 + ds_pipeline_2
